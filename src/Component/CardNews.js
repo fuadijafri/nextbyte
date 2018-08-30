fa849b64fd5eac52c4593973000e727e4f9348de
Fuadi Jafri <fuadijafri@gmail.com>
2018-08-30 15:02:24 +0700

    Initial Project

---
+++ src/Component/CardNews.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import { Card, Button } from 'antd';
+
+class CardNews extends Component {
+  render() {
+    return (
+      <Card 
+          style={{ width:250 , margin:20, textAlign:"center" }}
+          cover={this.props.cover}
+          actions={[<a href={this.props.recipeLink}><Button type="danger">View Recipe</Button></a>]}
+      >
+
+        <Card.Meta
+          title={this.props.title}
+        >
+        
+        </Card.Meta>
+      </Card> 
+    )
+  }
+}
+
+export default CardNews
---
+++ src/Component/Header.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+import {Menu, Icon} from 'antd'
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posisi: 'route1',
+    }
+  }
+
+  render() {
+    return (
+      <Menu
+        selectedKeys={[this.state.posisi]}
+        mode="horizontal"
+      >
+        <Menu.Item key="route1">
+          <Link to ='/all-in-food'>All in Food</Link>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
+export default Header
